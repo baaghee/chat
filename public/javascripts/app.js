8122efd1ee08bbe098287981fbcb79fb3b8ded0d
iuthisaam <iuthisaam@eww.(none)>
2013-01-28 20:58:32 +0500

    fixed dummy template

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 var socket = io.connect('/chat');
 
 socket.on('incoming', function(data){
-	$("#chat-window-container").append('<div class="media chat-item"><div class="chat-bubble-arrow"></div><a href="#" class="pull-left"><img data-src="holder.js/64x64" class="media-object"></a><div class="media"><a href="#" class="pull-left"><img data-src="holder.js/64x64" alt="64x64" src="'+data.user.photo+'" class="media-object"></a></div><div class="media-body"><a href="#" class="media-heading">Media heading</a><br><p>'+data.msg+'</p></div></div>');
+	$("#chat-window-container").append('<div class="media chat-item"><div class="chat-bubble-arrow"></div><a href="#" class="pull-left"><img data-src="holder.js/64x64" class="media-object"></a><div class="media"><a href="#" class="pull-left"><img data-src="holder.js/64x64" alt="64x64" src="'+data.user.photo+'" class="media-object"></a></div><div class="media-body"><a href="#" class="media-heading">'+data.user.name+'</a><br><p>'+data.msg+'</p></div></div>');
 	console.log(data)
 });
 
