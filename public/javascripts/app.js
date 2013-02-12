a376bb85220dc8e5ccb30dc0c0e8009ec081fc79
HiLARL <Hilarl@Hilarls-MacBook-Pro.local>
2013-02-12 23:35:24 +0500

    added status icon and new tweet button

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -79,7 +79,7 @@ $(function(){
 	$.getJSON('/friends-on-chat', function(res){
 		$("#my-instant-contacts").html("");
 		res.forEach(function(e){
-			$("#my-instant-contacts").append('<div id="list-'+e.id+'" data-id="'+e.id+'" data-name="'+e.name+'" class="media user-list-item"><a href="#" class="user-invisible-link pull-left"><img data-src="holder.js/64x64" class="media-object"></a><div class="media"><a href="#" class="pull-left"><img data-src="holder.js/64x64" alt="64x64" style="width: 64px; height: 64px;" src="'+e.pic+'" class="media-object"></a></div><div class="media-body"><a href="#" class="media-heading">'+e.name+'</a><br><span id="list-presence-'+e.id+'">'+(e.online == "yes" ? "online": "")+'</span></div><br class="clear"></div>');
+			$("#my-instant-contacts").append('<div id="list-'+e.id+'" data-id="'+e.id+'" data-name="'+e.name+'" class="media user-list-item"><a href="#" class="user-invisible-link pull-left"><img data-src="holder.js/64x64" class="media-object"></a><div class="media"><a href="#" class="pull-left"><img data-src="holder.js/64x64" alt="64x64" style="width: 64px; height: 64px;" src="'+e.pic+'" class="media-object"></a></div><div class="media-body"><a href="#" class="media-heading">'+e.name+'</a><br><span id="list-presence-'+e.id+'">'+(e.online == "yes" ? "<span class='online-icon'> </span>" : "") +'</span></div><br class="clear"></div>');
 		});
 	});
 	$("body").on("click", ".user-list-item", function(){
